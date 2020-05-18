b3d12d757b48b640a938d0272e1971e0d2f60527
James Mbabazi <jmbabazi@pih.org>
2020-05-17 22:27:45 -0400

    Scripts to install a Rwanda EMR environment

---
+++ src/main/resources/scripts/install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+## load variables from the env file
+source vars
+
+create_base_dir(){
+  mkdir -p ${Base_Dir}
+}
+
+download_maven_distribution(){
+    create_base_dir
+    mkdir -p ${Target_Dir}
+	  ./download-maven-artifact.sh --groupId=${Group_ID} --artifactId=${Artifact_ID} --version=${Version} --classifier=${Classifier} --type=${Type} --targetDir=${Target_Dir}
+	  unzip ${Target_Dir}/${Artifact_ID}-${Version}-${Classifier}.zip
+	  mv ${Artifact_ID}-${Version}/* ${Target_Dir}
+	  rm -rf ${Artifact_ID}-${Version}
+	  rm -rf ${Target_Dir}/${Artifact_ID}-${Version}-${Classifier}.zip
+}
+
+download_maven_distribution
+
