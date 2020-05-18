fc5a909860f7e89c160b96134a7084a6b1adeda8
James Mbabazi <jmbabazi@pih.org>
2020-05-18 10:11:46 -0400

    Adding download_maven_installer

--- src/main/resources/scripts/install.sh
+++ src/main/resources/scripts/install.sh
@@ -14,8 +14,21 @@ download_maven_distribution(){
 	  unzip ${Target_Dir}/${Artifact_ID}-${Version}-${Classifier}.zip
 	  mv ${Artifact_ID}-${Version}/* ${Target_Dir}
 	  rm -rf ${Artifact_ID}-${Version}
+
+}
+
+download_maven_installer(){
+	  create_base_dir
+	  Target_Dir=${Base_Dir}/${Installer_Dir}
+	  Version=${Installer_Version}
+	  Classifier=${Installer_classifier}
+	  Artifact_ID=${Installer_Artifact_ID}
+	  mkdir -p ${Target_Dir}
+	  ./download-maven-artifact.sh --groupId=${Group_ID} --artifactId=${Artifact_ID} --version=${Version} --classifier=${Classifier} --type=${Type} --targetDir=${Target_Dir}
+	  unzip ${Target_Dir}/${Artifact_ID}-${Version}-${Classifier}.zip
+	  mv ${Artifact_ID}-${Version}/* ${Target_Dir}
 	  rm -rf ${Target_Dir}/${Artifact_ID}-${Version}-${Classifier}.zip
 }
 
-download_maven_distribution
-
+#download_maven_distribution
+download_maven_installer
