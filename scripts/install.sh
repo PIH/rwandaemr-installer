da0cf6a5d493767a0b8903477b35e1396e3a2081
James Mbabazi <jmbabazi@pih.org>
2020-05-20 09:29:54 -0400

    Creating mysql Data_Dir variable

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,16 +3,11 @@
 ## load variables from the env file
 source vars
 
-create_base_dir(){
-    mkdir -p ${Base_Dir}
-}
-
 create_data_dir(){
-  mkdir -p ../data
+  mkdir -p ${Base_Dir}/${Data_Dir}
 }
 
 download_maven_distribution(){
-    create_base_dir
     mkdir -p ${Target_Dir}
 	  ./download-maven-artifact.sh --groupId=${Group_ID} --artifactId=${Artifact_ID} --version=${Version} --classifier=${Classifier} --type=${Type} --targetDir=${Target_Dir}
 	  unzip ${Target_Dir}/${Artifact_ID}-${Version}-${Classifier}.zip
